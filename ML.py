eb4822f15457a12bd2637bad7a3504be10c411d4
Alexander <sashavonsup@mail.ru>
2019-02-10 17:48:56 +0300

    Сделана первая рабочая версия!
    Единственное, что в стандартном примере кода теги table и tr не разбираются (пропадают вообще)

--- ML.py
+++ ML.py
@@ -9,30 +9,134 @@ from sys import stdin
 import re
 
 
-code = list(map(lambda x: x[:-1], stdin))
-html = ''  # В эту переменную записывается получающийся хтмлькод
+def is_word(s):
+    if not s[0].isalpha():
+        return False
+    if not s[1:].isalnum():
+        return False
+    return True
+
+
+def ERROR(code: int, s: str):
+    print()
+    print('ERROR ', code, ': ', s, sep='')
+    print()
+    exit(code)
+
+
+def is_attr_needed():
+    if attr:
+        ERROR(201, 'Attribute value expected')
+
+
+class Token:
+    def __init__(self, type, value=None):
+        self.type = type
+        self.val = value
+
+    def to_string(self):
+        if self.val is None:
+            return self.type
+        return self.type + ': ' + str(self.val)
+
+
+tokens = []
 single_tags = ['area', 'base', 'basefont', 'bgsound', 'br', 'col', 'command', 'embed', 'hr', 'img',
                'input', 'isindex', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr']  # Незакрывающиеся теги
+tags = ['!--...--', '!DOCTYPE', 'a', 'abbr', 'address', 'article', 'aside', 'audio', 'b', 'bdi', 'bdo', 'blockquote',
+        'body', 'button', 'canvas', 'caption', 'cite', 'code', 'colgroup', 'data', 'datalist', 'dd', 'del', 'details',
+        'dfn', 'dialog', 'div', 'dl', 'dt', 'em', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h1', 'h2',
+        'h3', 'h4', 'h5', 'h6', 'head', 'header', 'html', 'i', 'iframe', 'ins', 'kbd', 'label', 'legend', 'li', 'main',
+        'map', 'mark', 'meter', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'picture',
+        'pre', 'progress', 'q', 'ruby', 'rb', 'rt', 'rtc', 'rp', 's', 'samp', 'script', 'section', 'select', 'small',
+        'span', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot',
+        'th', 'thead', 'time', 'title', 'tr', 'u', 'ul', 'var', 'video']
+quotes = "'" + '"'
 h_sep = '\t'  # Разделитель иерархии
+spaces = ' \t\v\r\f'
+attr = False
 
 
-tags = []
-pag = -1
-for el in code:
-    line = el.lstrip(h_sep).strip()  # сама строка
-    lvl = (len(el) - len(line)) // len(h_sep)  # уровень вложенности
-    if lvl > pag:
-        pag += 1
+def check(text):
+    global tokens
+    global attr
+
+    if len(text) == 0:
+        pass
+    elif text in tags:
+        is_attr_needed()
+        tokens.append(Token(text, True))
+    elif text in single_tags:
+        is_attr_needed()
+        tokens.append(Token(text, False))
+    elif text[-1] == '=':
+        if is_word(text[:-1]):
+            tokens.append(Token('attr_name', text[:-1]))
+            attr = True
+        else:
+            ERROR(102, 'Illegal attribute: ' + text)
+
+
+def tokenize_text(text):
+    global attr
+    global tokens
+    if attr:
+        tokens.append(Token('attr_val', text))
+        attr = False
     else:
-        for i in range(pag - lvl + 1):
-            if tags[-1] not in single_tags:
-                html += '</'+tags.pop()+'>'
-        pag += lvl - pag
-
-    html += '<'+line+'>'
-    tags.append(line.split()[0])
-
-html += ''.join(list(map(lambda x: '</'+x+'>', reversed(tags))))
+        tokens.append(Token('TEXT', text))
 
 
-print(html)
+def lexer(code):
+    global tokens
+    pag = -1
+    for el in code:
+        line = el.lstrip(h_sep).strip()  # сама строка
+        lvl = (len(el) - len(line)) // len(h_sep)  # уровень вложенности
+        if lvl > pag:
+            pag += 1
+        else:
+            is_attr_needed()
+            tokens += [Token('EOT')] * (pag - lvl + 1)  # End of tag
+
+        string = False
+        quote = ""
+        buffer = ''
+        i = 0
+        while i < len(line):
+            if string:
+                if line[i] == quote:
+                    string = False
+                    tokenize_text(buffer)
+                    buffer = ''
+                else:
+                    buffer += line[i]
+            elif line[i] in quotes:
+                    string = True
+                    quote = line[i]
+                    check(buffer)
+                    buffer = ''
+            elif line[i] in spaces:
+                while i < len(line) and line[i+1] in spaces:
+                    i += 1
+                check(buffer)
+                buffer = ''
+            elif line[i] == '=':
+                buffer += '='
+                check(buffer)
+                buffer = ''
+            else:
+                buffer += line[i]
+            i += 1
+
+
+if __name__ == '__main__':
+    # code = list(map(lambda x: x[:-1], stdin))
+    code = ['@']
+    while code[-1] != '':
+        code.append(input())
+    code = code[1:-1]
+
+    html = ''  # В эту переменную записывается получающийся хтмлькод
+    lexer(code)
+    print(*[i.to_string() for i in tokens], sep='\n')
